987d9d673b02a71d7a45994e0a432aa47b1375cf
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-07-29 12:42:04 -0300

    ParkingSpotService completed !

--- src/main/java/br/com/matsutech/parkingcontrol/service/ParkingSpotService.java
+++ src/main/java/br/com/matsutech/parkingcontrol/service/ParkingSpotService.java
@@ -1,5 +1,9 @@
 package br.com.matsutech.parkingcontrol.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,18 +23,28 @@ public class ParkingSpotService {
         return repository.save(parkingSpot);
     }
 
+    public List<ParkingSpot> findAll() {
+        return repository.findAll();
+    }
 
     public boolean existsByLicensePlateCar(String licensePlateCar) {
         return repository.existsByLicensePlateCar(licensePlateCar);
     }
 
-  
     public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
         return repository.existsByParkingSpotNumber(parkingSpotNumber);
     }
 
-
     public boolean existsByApartmentAndBlock(String apartment, String block) {
         return repository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public Optional<ParkingSpot> findParkingSpotById(UUID id) {
+        return repository.findById(id);
+    }
+    
+    @Transactional
+    public void deleteParkingSpotById(ParkingSpot parkingSpot) {
+        repository.delete(parkingSpot);
     }
 }
